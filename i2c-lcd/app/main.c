633fe54e9b03a2f5444f61cd817aaf225f13aa5c
Gabriella Lord <gabriellalord@icloud.com>
2025-04-28 17:22:49 -0600

    Basic structure for LCD, arrows, english comments

--- i2c-lcd/app/main.c
+++ i2c-lcd/app/main.c
@@ -1,6 +1,6 @@
 /*
- * EELE 465, Project 5
- * Gabby and Iker
+ * EELE 465, Final Project
+ * Gabby Lord
  *
  * Target device: MSP430FR2310 Slave
  */
@@ -16,22 +16,22 @@
 //----------------------------------------------------------------------
 // Definitions
 //----------------------------------------------------------------------
-// Puerto 2
-#define RS BIT0     // P2.0
-#define EN BIT6     // P2.6
+// Port 2
+#define RS BIT0     // P2.0 - Register Select
+#define EN BIT6     // P2.6 - Enable
 
-// Puerto 1
-#define D4  BIT4     // P1.4
-#define D5 BIT5     // P1.5
-#define D6 BIT6     // P1.6
-#define D7 BIT7     // P1.7
-#define SLAVE_ADDR  0x38                    // Slave I2C Address
+// Port 1
+#define D4  BIT4    // P1.4 - LCD Data 4
+#define D5 BIT5     // P1.5 - LCD Data 5
+#define D6 BIT6     // P1.6 - LCD Data 6
+#define D7 BIT7     // P1.7 - LCD Data 7
+#define SLAVE_ADDR  0x38   // Slave I2C Address
 //--End Definitions-----------------------------------------------------
 
 //----------------------------------------------------------------------
 // Variables
 //----------------------------------------------------------------------
-volatile unsigned char receivedData = 0;    // Recieved data
+volatile unsigned char receivedData = 0;    // Received data from I2C
 char key_unlocked;
 char mode = '\0';
 char prev_mode = '\0';
@@ -41,6 +41,91 @@ int length_time = 0;
 int length_adc = 0;
 bool in_time_mode = false;
 bool in_temp_adc_mode = false;
+
+// Custom characters for LCD
+// Arrow pointing North (↑)
+unsigned char arrow_north[8] = {
+    0b00000,
+    0b00000,
+    0b00100,
+    0b01110,
+    0b10101,
+    0b00100,
+    0b00100,
+    0b00000
+};
+
+// Arrow pointing Northeast (↗)
+unsigned char arrow_ne[8] = {
+    0b00000,
+    0b00000,
+    0b00111,
+    0b00011,
+    0b00101,
+    0b01000,
+    0b10000,
+    0b00000
+};
+
+// Arrow pointing Southeast (↘)
+unsigned char arrow_se[8] = {
+    0b00000,
+    0b00000,
+    0b10000,
+    0b01000,
+    0b00101,
+    0b00011,
+    0b00111,
+    0b00000
+};
+
+// Arrow pointing South (↓)
+unsigned char arrow_south[8] = {
+    0b00000,
+    0b00000,
+    0b00100,
+    0b00100,
+    0b10101,
+    0b01110,
+    0b00100,
+    0b00000
+};
+
+// Arrow pointing Southwest (↙)
+unsigned char arrow_sw[8] = {
+    0b00000,
+    0b00000,
+    0b00001,
+    0b00010,
+    0b10100,
+    0b11000,
+    0b11100,
+    0b00000
+};
+
+// Arrow pointing Northwest (↖)
+unsigned char arrow_nw[8] = {
+    0b00000,
+    0b00000,
+    0b11100,
+    0b11000,
+    0b10100,
+    0b00010,
+    0b00001,
+    0b00000
+};
+
+// Angle symbol for LCD
+unsigned char angle[8] = {
+    0b00000,
+    0b00000,
+    0b00001,
+    0b00010,
+    0b00100,
+    0b01000,
+    0b11111,
+    0b00000
+};
 //--End Variables-------------------------------------------------------
 
 //----------------------------------------------------------------------
@@ -54,213 +139,161 @@ void I2C_Slave_Init(void)
     P1SEL1 &= ~(BIT2 | BIT3);
     P1SEL0 |= BIT2 | BIT3;
 
-    // Configure USCI_B0 as I2C Slave
+    // Configure USCI_B0 for I2C Slave mode
     UCB0CTLW0 |= UCSWRST;               // Put eUSCI_B0 into software reset
-    UCB0CTLW0 |= UCMODE_3;              // Select I2C slave mode
-    UCB0I2COA0 = SLAVE_ADDR + UCOAEN;   // Set and enable first own address
-    UCB0CTLW0 |= UCTXACK;               // Send ACKs
+    UCB0CTLW0 |= UCMODE_3;              // I2C mode, slave
+    UCB0I2COA0 = SLAVE_ADDR + UCOAEN;   // Own address enable
+    UCB0CTLW0 |= UCTXACK;               // Automatically send ACK
 
-    PM5CTL0 &= ~LOCKLPM5;               // Disable low-power inhibit mode
+    PM5CTL0 &= ~LOCKLPM5;               // Disable GPIO high-impedance mode
 
-    UCB0CTLW0 &= ~UCSWRST;              // Pull eUSCI_B0 out of software reset
+    UCB0CTLW0 &= ~UCSWRST;              // Release eUSCI_B0 from reset
     UCB0IE |= UCSTTIE + UCRXIE;         // Enable Start and RX interrupts
 
-    __enable_interrupt();               // Enable Maskable IRQs
+    __enable_interrupt();               // Enable global interrupts
 }
-//--End I2C Init--------------------------------------------------------
+//--End I2C Init---------------------------------------------------------
 
 //----------------------------------------------------------------------
 // Begin Send Commands
 //----------------------------------------------------------------------
 void pulseEnable() {
-    P2OUT |= EN;             // Establecer Enable en 1
-    __delay_cycles(1000);    // Retardo
-    P2OUT &= ~EN;            // Establecer Enable en 0
-    __delay_cycles(1000);    // Retardo
+    P2OUT |= EN;             // Set Enable high
+    __delay_cycles(1000);    // Small delay
+    P2OUT &= ~EN;            // Set Enable low
+    __delay_cycles(1000);    // Small delay
 }
 
 void sendNibble(unsigned char nibble) {
-    P1OUT &= ~(D4 | D5 | D6 | D7);  // Limpiar los bits de datos
-    P1OUT |= ((nibble & 0x0F) << 4);  // Cargar el nibble en los bits correspondientes (P1.4 a P1.7)
-    pulseEnable();  // Pulsar Enable para enviar datos
+    P1OUT &= ~(D4 | D5 | D6 | D7);  // Clear data lines
+    P1OUT |= ((nibble & 0x0F) << 4);  // Output nibble
+    pulseEnable();  // Pulse Enable to latch data
 }
 
 void send_data(unsigned char data) {
-    P2OUT |= RS;    // Modo datos
-    sendNibble(data >> 4);  // Enviar los 4 bits más significativos
-    sendNibble(data & 0x0F);  // Enviar los 4 bits menos significativos (corregido)
-    __delay_cycles(4000); // Retardo para procesar los datos
+    P2OUT |= RS;    // Data mode
+    sendNibble(data >> 4);  // Send high nibble
+    sendNibble(data & 0x0F);  // Send low nibble
+    __delay_cycles(4000); // Delay for data write
 }
 
 void send_command(unsigned char cmd) {
-    P2OUT &= ~RS;   // Modo comando
-    sendNibble(cmd >> 4);  // Enviar los 4 bits más significativos
-    sendNibble(cmd);  // Enviar los 4 bits menos significativos
-    __delay_cycles(4000); // Retardo para asegurarse de que el comando se procese
+    P2OUT &= ~RS;   // Command mode
+    sendNibble(cmd >> 4);  // Send high nibble
+    sendNibble(cmd);  // Send low nibble
+    __delay_cycles(4000); // Delay for command processing
 }
 
 void lcdSetCursor(unsigned char position) {
-    send_command(0x80 | position);  // Establecer la dirección del cursor en la DDRAM
+    send_command(0x80 | position);  // Set cursor to DDRAM address
+}
+
+void lcd_print(const char* str, unsigned char startPos) {
+    lcdSetCursor(startPos);
+    while (*str) {
+        send_data(*str++);
+        startPos++;
+        if (startPos == 0x10) startPos = 0x40;  // Jump to second line after end of first
+    }
+}
+
+void lcd_create_char(unsigned char location, unsigned char charmap[]) {
+    location &= 0x07; // Only locations 0–7 are valid
+    send_command(0x40 | (location << 3)); // Set CGRAM address
+    int i;
+    for (i = 0; i < 8; i++) {
+        send_data(charmap[i]);
+    }
 }
 //--End Send Commands---------------------------------------------------
 
 //----------------------------------------------------------------------
-// Begine LCD Init
+// Begin LCD Init
 //----------------------------------------------------------------------
 void lcdInit() {
-    // Configurar pines como salida
+    // Set pins as output
     P1DIR |= D4 | D5 | D6 | D7;
     P2DIR |= RS | EN;
 
-    // Limpiar salidas
+    // Clear outputs
     P1OUT &= ~(D4 | D5 | D6 | D7);
     P2OUT &= ~(RS | EN);
-    __delay_cycles(50000);  // Retardo de inicio
-    sendNibble(0x03);  // Inicialización del LCD
-    __delay_cycles(5000);  // Retardo
-    sendNibble(0x03);  // Repetir la inicialización
-    __delay_cycles(200);  // Retardo
-    sendNibble(0x03);  // Repetir la inicialización
-    sendNibble(0x02);  // Establecer modo de 4 bits
+    __delay_cycles(50000);  // Initial delay
+    sendNibble(0x03);  // LCD initialization sequence
+    __delay_cycles(5000);
+    sendNibble(0x03);
+    __delay_cycles(200);
+    sendNibble(0x03);
+    sendNibble(0x02);  // Set 4-bit mode
 
-    send_command(0x28);  // Configurar LCD: 4 bits, 2 líneas, 5x8
-    send_command(0x0C);  // Encender display, apagar cursor
-    send_command(0x06);  // Modo de escritura automática
-    send_command(0x01);  // Limpiar la pantalla
-    __delay_cycles(2000); // Esperar para limpiar la pantalla
+    send_command(0x28);  // 4-bit, 2-line, 5x8 font
+    send_command(0x0C);  // Display ON, cursor OFF
+    send_command(0x06);  // Auto-increment cursor
+    send_command(0x01);  // Clear display
+    __delay_cycles(2000); // Wait for clear
+
+    // Load custom characters
+    lcd_create_char(0, arrow_north);
+    lcd_create_char(1, arrow_ne);
+    lcd_create_char(2, arrow_se);
+    lcd_create_char(3, arrow_south);
+    lcd_create_char(4, arrow_sw);
+    lcd_create_char(5, arrow_nw);
+    lcd_create_char(6, angle);
+
+    // Initial display text
+    lcd_print(" xxx.x", 0x00);
+    lcd_print("TDOA:xx s", 0x40);
+
+    // Place angle and degree symbols
+    lcdSetCursor(0x00);
+    send_data(0x06);        // Custom angle symbol
+    lcdSetCursor(0x06);
+    send_data(0xDF);        // Built-in degree symbol
+    lcdSetCursor(0x47);
+    send_data(0xE4);        // Built-in mu (μ) symbol
 }
 //--End LCD Init--------------------------------------------------------
 
 //----------------------------------------------------------------------
 // Begin Print Commands
 //----------------------------------------------------------------------
-void lcd_print(const char* str, unsigned char startPos) {
-    lcdSetCursor(startPos);
-    while (*str) {
-        send_data(*str++);
-        startPos++;
-        if (startPos == 0x10) startPos = 0x40;  // Salto automático a segunda línea
-    }
-}
-
-void display_time(char input)
-{
-    char string[2];
-    string[0] = input;
-    string[1] = '\0';
-    switch (length_time) {
-        case 0:
-            length_time++;
-            break;
-        case 1:
-            lcd_print(string, 0x42);
-            length_time++;
-            break;
-        case 2:
-            lcd_print(string, 0x43);
-            length_time++;
-            break;
-        case 3:
-            lcd_print(string, 0x44);
-            in_time_mode = false;
-            mode = 'A';
-            length_time = 0;
-            break;
-        default:
-            in_time_mode = false;
-            length_time = 0;
-            break;
-    }
-}
-
-void display_temp_adc(char input)
-{
-    char string[2];
-    string[0] = input;
-    string[1] = '\0';
-    switch (length_adc) {
-        case 0:
-            length_adc++;
-            break;
-        case 1:
-            lcd_print(string, 0x0A);
-            length_adc++;
-            break;
-        case 2:
-            lcd_print(string, 0x0B);
-            length_adc++;
-            break;
-        case 3:
-            lcd_print(string, 0x0D);
-            in_temp_adc_mode = false;
-            mode = 'A';
-            length_adc = 0;
-            break;
-        default:
-            in_temp_adc_mode = false;
-            length_adc = 0;
-            break;
-    }
-}
-
 void display_output(char input)
 {
     switch (input)
     {
-        case 'A':           // heat mode
-            lcd_print("HEAT ", 0x00);
-            mode = 'A';
+        case '1':           // North
+            lcdSetCursor(0x4F);
+            send_data(0x00);
             break;
-        case 'B':           // cool mode
-            lcd_print("COOL ", 0x00);
-            mode = 'B';
+        case '2':           // Northeast
+            lcdSetCursor(0x4F);
+            send_data(0x01);
             break;
-        case 'C':           // match mode
-            lcd_print("MATCH", 0x00);
-            mode = 'C';
+        case '3':           // East
+            lcdSetCursor(0x4F);
+            send_data(0x7E); // Built-in right arrow
             break;
-        case 'D':           // off
-            send_command(0x01);
-            lcd_print("OFF  ", 0x00);
+        case '4':           // Southeast
+            lcdSetCursor(0x4F);
+            send_data(0x02);
             break;
-        case 'S':           // display seconds
-            in_time_mode = true;
-            //prev_mode = mode;
-            mode = 'S';
-            length_time = 0;
+        case '5':           // South
+            lcdSetCursor(0x4F);
+            send_data(0x03);
             break;
-        case 'X':           // display I2C temperature
+        case '6':           // Southwest
+            lcdSetCursor(0x4F);
+            send_data(0x04);
             break;
-        case 'Y':           // display AD2 temperature
-            in_temp_adc_mode = true;
-            //prev_mode = mode;
-            mode = 'Y';
-            length_adc = 0; // reset position for temperature digits
+        case '7':           // West
+            lcdSetCursor(0x4F);
+            send_data(0x7F); // Built-in left arrow
             break;
-        case 'Z':           // just unlocked
-            send_command(0x01);
-            __delay_cycles(2000);
-            lcd_print("OFF     A:xx.x", 0x00);
-            lcdSetCursor(0x0E);             // Move to where the degree symbol goes
-            send_data(0xDF);                // Send the built-in degree symbol
-            lcd_print("C", 0x0F);           // Continue with 'C'
-            lcd_print("3 xxxs  P:xx.x", 0x40);
-            lcdSetCursor(0x4E);             // Move to where the degree symbol goes
-            send_data(0xDF);                // Send the built-in degree symbol
-            lcd_print("C", 0x4F);           // Continue with 'C'
-            mode = 'A';
+        case '8':           // Northwest
+            lcdSetCursor(0x4F);
+            send_data(0x05);
             break;
-    }
-
-    if (mode == 'S')
-    {
-        display_time(input);
-    }
-
-    
-    if (mode == 'Y')
-    {
-        display_temp_adc(input);
     }
 }
 //--End Print Commands--------------------------------------------------
@@ -269,11 +302,11 @@ void display_output(char input)
 // Begin Main
 //----------------------------------------------------------------------
 int main(void) {
-    WDTCTL = WDTPW | WDTHOLD;  // Detener el watchdog
-    PM5CTL0 &= ~LOCKLPM5;
-    lcdInit();  // Inicializar el LCD
-    I2C_Slave_Init();                   // Initialize the slave for I2C
-    __bis_SR_register(LPM0_bits + GIE); // Enter LPM0, enable interrupts
+    WDTCTL = WDTPW | WDTHOLD;  // Stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;      // Disable GPIO high-impedance mode
+    lcdInit();                 // Initialize LCD
+    I2C_Slave_Init();           // Initialize I2C slave
+    __bis_SR_register(LPM0_bits + GIE); // Enter Low Power Mode 0 with interrupts enabled
     return 0;
 }
 //--End Main------------------------------------------------------------
@@ -287,8 +320,8 @@ __interrupt void USCI_B0_ISR(void)
 {
     switch (__even_in_range(UCB0IV, USCI_I2C_UCTXIFG0))
     {
-        case USCI_I2C_UCRXIFG0:         // Receive Interrupt
-            display_output(UCB0RXBUF);
+        case USCI_I2C_UCRXIFG0:         // Receive interrupt
+            display_output(UCB0RXBUF);  // Display received data
             break;
         default: 
             break;
